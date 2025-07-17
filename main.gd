72e4deeb3bb7838df365e97186655c3f2999c5c0
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-17 09:46:51 -0300

    feat: add npc select card

--- main.gd
+++ main.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
+@onready var npc_hand = $npc_hand
 @onready var end_turn_button := $CanvasLayer/EndTurnButton
+@onready var merge_button = $CanvasLayer2/MergeButton
 
 func _ready():
 	end_turn_button.visible = false
@@ -11,8 +13,18 @@ func _process(delta):
 		end_turn_button.visible = true
 
 func _on_end_turn_pressed():
-	print("Fim do turno do jogador.")
-	#valores resetados
-	end_turn_button.visible = false
-	GameState.played_card = false
-	GameState.selected_card = null
+	print("Jogador finalizou turno!")
+
+	# Escolher carta aleatória do NPC
+	var npc_cards = npc_hand.get_children()
+	if npc_cards.is_empty():
+		print("NPC não tem cartas")
+		return
+
+	var npc_card = npc_cards[randi() % npc_cards.size()]
+	npc_card.global_position = Vector3(-0.100, 0.559, 0.579)
+
+	# Mostrar botão "Fundir"
+	merge_button.visible = true
+
+	end_turn_button.disabled = true
