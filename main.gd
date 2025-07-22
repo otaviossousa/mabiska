023a9784eb6a536046d377b3ad3d90a9a3ebdddf
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-22 16:33:11 -0300

    fix: importing texture card file

--- main.gd
+++ main.gd
@@ -12,66 +12,7 @@ var npc_deck_elements = ["fire", "water", "earth", "air"]
 #import cards scenes
 @onready var card_base_scene := preload("res://deck scenes/card_base.tscn")
 
-@onready var card_textures = {
-	# bases
-	"fire": preload("res://assets/cards-assets/base_cards/20_Element_Fire.png"),
-	"water": preload("res://assets/cards-assets/base_cards/23_Element_Water.png"),
-	"earth": preload("res://assets/cards-assets/base_cards/25_Element_Earth.png"),
-	"air": preload("res://assets/cards-assets/base_cards/22_Element_Air.png"),
-	
-	# secondary
-	"sand": preload("res://assets/cards-assets/secondary_cards/Element_Sand.png"),
-	"lightning": preload("res://assets/cards-assets/secondary_cards/Element_Lightning.png"),
-	"mist": preload("res://assets/cards-assets/secondary_cards/Element_Mist.png"),
-	"magma": preload("res://assets/cards-assets/secondary_cards/Element_Magma.png"),
-	"clay": preload("res://assets/cards-assets/secondary_cards/Element_Clay.png"),
-	"steam": preload("res://assets/cards-assets/secondary_cards/Element_Steam.png"),
-	
-	#third
-	"ash": preload("res://assets/cards-assets/terciary_cards/Element_Ash.png"),
-	"basalt": preload("res://assets/cards-assets/terciary_cards/Element_Basalt.png"),
-	"brick": preload("res://assets/cards-assets/terciary_cards/Element_Brick.png"),
-	"charged mist": preload("res://assets/cards-assets/terciary_cards/Element_ChargedMist.png"),
-	"engine": preload("res://assets/cards-assets/terciary_cards/Element_Engine.png"),
-	"frosted glass": preload("res://assets/cards-assets/terciary_cards/Element_FrostedGlass.png"),
-	"geyser": preload("res://assets/cards-assets/terciary_cards/Element_Geyser.png"),
-	"glass": preload("res://assets/cards-assets/terciary_cards/Element_Glass.png"),
-	"golem": preload("res://assets/cards-assets/terciary_cards/Element_Golem.png"),
-	"magnetite": preload("res://assets/cards-assets/terciary_cards/Element_Magnetite.png"),
-	"mud": preload("res://assets/cards-assets/terciary_cards/Element_Mud.png"),
-	"obsidian": preload("res://assets/cards-assets/terciary_cards/Element_Obsidian.png"),
-	"radiance": preload("res://assets/cards-assets/terciary_cards/Element_Radiance.png"),
-	"smoke": preload("res://assets/cards-assets/terciary_cards/Element_Smoke.png"),
-	"swamp": preload("res://assets/cards-assets/terciary_cards/Element_Swamp.png"),
-	
-	#fourth
-	"blade": preload("res://assets/cards-assets/quartenary_cards/Element_Blade.png"),
-	"ceramic": preload("res://assets/cards-assets/quartenary_cards/Element_Ceramic.png"),
-	"cinder": preload("res://assets/cards-assets/quartenary_cards/Element_Cinder.png"),
-	"cloud": preload("res://assets/cards-assets/quartenary_cards/Element_Cloud.png"),
-	"crystal": preload("res://assets/cards-assets/quartenary_cards/Element_Crystal.png"),
-	"dark mirror": preload("res://assets/cards-assets/quartenary_cards/Element_DarkMirror.png"),
-	"fog": preload("res://assets/cards-assets/quartenary_cards/Element_Fog.png"),
-	"frost": preload("res://assets/cards-assets/quartenary_cards/Element_Frost.png"),
-	"furnace": preload("res://assets/cards-assets/quartenary_cards/Element_Furnace.png"),
-	"guardian": preload("res://assets/cards-assets/quartenary_cards/Element_Guardian.png"),
-	"halo": preload("res://assets/cards-assets/quartenary_cards/Element_Halo.png"),
-	"idol": preload("res://assets/cards-assets/quartenary_cards/Element_Idol.png"),
-	"mill": preload("res://assets/cards-assets/quartenary_cards/Element_Mill.png"),
-	"mirror": preload("res://assets/cards-assets/quartenary_cards/Element_Mirror.png"),
-	"prism": preload("res://assets/cards-assets/quartenary_cards/Element_Prism.png"),
-	"shard": preload("res://assets/cards-assets/quartenary_cards/Element_shard.png"),
-	"solar flame": preload("res://assets/cards-assets/quartenary_cards/Element_SolarFlame.png"),
-	"spirit golem": preload("res://assets/cards-assets/quartenary_cards/Element_SpiritGolem.png"),
-	"stained glass": preload("res://assets/cards-assets/quartenary_cards/Element_Stained Glass.png"),
-	"storm stone": preload("res://assets/cards-assets/quartenary_cards/Element_Stormstone.png"),
-	"tears": preload("res://assets/cards-assets/quartenary_cards/Element_Tears.png"),
-	"basalt dust": preload("res://assets/cards-assets/quartenary_cards/Element_BasaltDust.png"),
-	"thunder": preload("res://assets/cards-assets/quartenary_cards/Element_Thunder.png"),
-	"velarim": preload("res://assets/cards-assets/quartenary_cards/Element_Velarim.png"),
-	"aureol": preload("res://assets/cards-assets/quartenary_cards/Element_Aureolo.png"),
-	"silhain": preload("res://assets/cards-assets/quartenary_cards/Element_Silhain.png")
-}
+const CardTextures = preload("res://data/texture_cards.gd")
 
 const FusionTable = preload("res://data/fusion_cards.gd")
 
@@ -182,10 +123,12 @@ func _add_card_to_hand(hand: Node3D, element: String):
 	var mat = mesh.get_active_material(0).duplicate()
 	mesh.set_surface_override_material(0, mat)
 
-	if mat and card_textures.has(element):
-		mat.albedo_texture = card_textures[element]
+	var texture = CardTextures.get_texture(element)
+	if mat and texture:
+		mat.albedo_texture = texture
 	else:
 		print("⚠️ Textura não encontrada para: ", element)
+
 
 	hand.add_child(card)
 
@@ -221,10 +164,12 @@ func _initialize_hand(hand_node: Node3D, elements: Array, start_positions: Array
 		var mat = mesh.get_active_material(0).duplicate()
 		mesh.set_surface_override_material(0, mat)
 
-		if mat and card_textures.has(element):
-			mat.albedo_texture = card_textures[element]
+		var texture = CardTextures.get_texture(element)
+		if mat and texture:
+			mat.albedo_texture = texture
 		else:
 			print("⚠️ Textura não encontrada para: ", element)
+
 
 
 		hand_node.add_child(card)
