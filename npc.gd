acd08952e5a86558b521b361d4b0660528e01d8f
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-07-25 17:34:34 -0300

    feat: npc script added

--- npc.gd
+++ npc.gd
@@ -1,8 +1,5 @@
 extends Area3D
 
-@onready var frog_sfx_1: AudioStreamPlayer = $frog_sfx_1
-@onready var frog_sfx_2: AudioStreamPlayer = $frog_sfx_2
-
 var mesh: MeshInstance3D
 
 func _ready():
@@ -11,19 +8,13 @@ func _ready():
 	connect("mouse_exited", _on_mouse_exited)
 
 func _on_mouse_entered():
-	print("Mouse sobre o player")
+	print("Mouse sobre o npc")
 	
 func _on_mouse_exited():
-	print("Mouse saiu do player")
+	print("Mouse saiu do npc")
 	
 func _input_event(camera, event, event_position, normal, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			print("Clicou!")
-			#aleatoriedade
-			var play_num = randi_range(1,2)
-			if play_num == 2:
-				frog_sfx_1.play()
-			else:
-				frog_sfx_2.play(1.0)
 			
